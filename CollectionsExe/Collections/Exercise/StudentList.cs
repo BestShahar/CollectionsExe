eda1ef6f90e0be3f00a9b242e8158dcdacd42a51
User <User@LAB3-3>
2023-10-30 13:03:42 +0200

    3

--- CollectionsExe/Collections/Exercise/StudentList.cs
+++ CollectionsExe/Collections/Exercise/StudentList.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using CollectionsExe.Collections.Exercise.Models;
 
 namespace CollectionsExe.Collections.Exercise
@@ -14,7 +16,9 @@ namespace CollectionsExe.Collections.Exercise
 
         //Targil 1 - write the Property of StudentList called Students
         #region Properties
-       
+        public List<Student> Students { get; set; }
+
+      
 
         #endregion
 
@@ -22,7 +26,7 @@ namespace CollectionsExe.Collections.Exercise
         #region Constructor
         public StudentList()
         {
-
+            Students=Student.FillList();
         }
         #endregion
 
@@ -33,16 +37,30 @@ namespace CollectionsExe.Collections.Exercise
         //Targil 3- write a method which retrieves a Student By name. 
 
         public Student FindStudentByName(string name)
+
         {
+            for(int i=0; i< Students.Count; i++)
+            {
+                if (Students[i].Name==name)
+                    return Students[i];
+            }
             return null;
         }
         #endregion
 
         #region retrieving a list
         //Targil 4-  write a method which retrieves a list of Students By Kita
-        public List<Student> GetStudentsByKits(string kita)
+        public List<Student> GetStudentsByKits(int kita)
+
         {
-            return null;
+            List<Student> list = new List<Student>();
+            for (int i = 0; i < Students.Count; i++)
+            {
+                if (Students[i].Kita == kita)
+
+                    list.Add(Students[i]);
+            }
+            return list;
         }
         #endregion
 
